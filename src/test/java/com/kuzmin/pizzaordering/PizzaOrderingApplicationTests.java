9660b88868c780e238f93adccf8b616fe79ea6fc
kuzminal <kuzmin.al35@gmail.com>
2021-01-26 10:40:50 +0300

    Initial commit

---
+++ src/test/java/com/kuzmin/pizzaordering/PizzaOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuzmin.pizzaordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
