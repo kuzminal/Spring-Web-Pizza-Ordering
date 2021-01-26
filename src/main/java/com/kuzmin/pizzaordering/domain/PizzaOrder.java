a98573fed1276dd171959cd6bc9784f137329422
kuzminal <kuzmin.al35@gmail.com>
2021-01-26 13:28:17 +0300

    Added controller and domain classes Added bootstrap and thymeleaf fragments

---
+++ src/main/java/com/kuzmin/pizzaordering/domain/PizzaOrder.java
@@ -0,0 +1,23 @@
+package com.kuzmin.pizzaordering.domain;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class PizzaOrder {
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+    private List<Pizza> pizzas = new ArrayList<>();
+
+    public void addPizza(Pizza pizza) {
+        this.pizzas.add(pizza);
+    }
+}
