6cd30841546b60f48bd10e6872ad7e12b7425e26
kuzminal <kuzmin.al35@gmail.com>
2021-01-28 14:31:17 +0300

    Added order form and translation

---
+++ src/main/java/com/kuzmin/pizzaordering/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.kuzmin.pizzaordering.controller;
+
+import com.kuzmin.pizzaordering.domain.PizzaOrder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("pizzaOrder", new PizzaOrder());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(PizzaOrder order) {
+        log.info("Order submitted: " + order);
+        return "redirect:/";
+    }
+}
